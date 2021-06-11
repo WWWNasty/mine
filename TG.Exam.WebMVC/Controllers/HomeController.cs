c94758961afee7ec578481b0c6bf4a368206eddc
gryshenko <gryshenko@payberry.ru>
2021-06-12 01:02:03 +0300

    Start of mvc task.

--- TG.Exam.WebMVC/Controllers/HomeController.cs
+++ TG.Exam.WebMVC/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+ using  TG.Exam.WebMVC.Models;
 
 namespace TG.Exam.WebMVC.Controllers
 {
@@ -10,7 +11,8 @@ namespace TG.Exam.WebMVC.Controllers
     {
         public ActionResult Index()
         {
-            return View();
+            var data = TG.Exam.WebMVC.Models.User.GetAll();
+            return View(data);
         }
 
         public ActionResult About()
