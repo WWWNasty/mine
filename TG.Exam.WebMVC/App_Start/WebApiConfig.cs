9b3317c4a5f8e28cc46d1e8e85f61d5de0a7ad5c
WWWNasty <nas.grischenk@yandex.ru>
2021-06-12 23:08:19 +0300

    Added task mvc.

--- TG.Exam.WebMVC/App_Start/WebApiConfig.cs
+++ TG.Exam.WebMVC/App_Start/WebApiConfig.cs
@@ -13,7 +13,7 @@ namespace TG.Exam.WebMVC
 
             // Web API routes
             config.MapHttpAttributeRoutes();
-
+          
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "api/{controller}/{id}",
---
+++ TG.Exam.WebMVC/Scripts/getData.js
@@ -0,0 +1,18 @@
+﻿let getAllAsync = $('#getAllAsync');
+
+getAllAsync.click(async ()=>{
+    let response = await fetch("/api/UsersApi/Get");
+    const users = await response.json();
+    let usersData = $('#usersData');
+    usersData.empty();
+
+    const usersRows = users.map((user, i) =>
+        `<tr>
+            <td>${i + 1}</td>
+            <td>${user.FirstName}</td>
+            <td>${user.LastName}</td>
+            <td>${user.Age + 10}</td>
+            <td>Async</td>
+         </tr>`).join('');
+    usersData.append(usersRows);
+});
